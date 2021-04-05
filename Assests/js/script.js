6fccc0bf9fd9929afa1dfa6b4dc3716345b69f27
MalinduAmesh <66924443+Malinducj@users.noreply.github.com>
2021-04-06 00:38:29 +0530

    v1.2

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -21,6 +21,27 @@
 //     });
 //   })
 
+const btn = document.querySelectorAll(".category button");
+const product = document.querySelectorAll(".itemBox");
+
+btn.forEach(item=> {
+    item.addEventListener("click",function(){
+        for(let i=0;i<btn.length;i++){
+            btn[i].classList.remove("active");
+        }
+        item.classList.add("active");
+        
+        product.forEach(show=>{
+            show.style.display = "none";
+            let products = item.textContent.toLowerCase();
+            if(show.getAttribute("data-att")=== products || products ==="all"){
+                show.style.display="block";
+            }
+        })
+    })
+} )
+
+
 var typed = new Typed('.animattte', {
     strings: ['Developer', 'Photographer','Designer'],
   
